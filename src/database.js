2f8a92db6043327c6776aaf32568dbdb50bd5fb5
Yuri Yakymiv <ypyakymiv@gmail.com>
2022-10-03 01:09:53 -0400

    feature: create, get, and delete users

--- src/database.js
+++ src/database.js
@@ -1,25 +1,9 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://parkingspotdetectorapi:password@127.0.0.1:27017/parkingspotdetector"; //process.env.DATABASE_URL;
+const URI = "mongodb://parkingspotdetectorapi:password@127.0.0.1:27017/parkingspotdetector"; //process.env.DATABASE_URL;
 
-async function main() {
-  await mongoose.connect(uri);
-
-  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
-
-  const UserSchema = new mongoose.Schema({
-    name: String
-  });
-
-  const User = new mongoose.model('User', UserSchema);
-
-  const new_user = new User({name: "yuriyakymiv"});
-
-  await new_user.save();
-
-  const users = await User.find();
-
-  console.log(users);
+async function connect_to_db() {
+  return mongoose.connect(URI);
 }
 
-main().catch(err => console.log(err));
+module.exports = connect_to_db;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const resources = require('./resources');
-const { PrismaClient } = require('@prisma/client');
+const { MongoServerError } = require('mongodb');
 
-const prisma = new PrismaClient();
-
-prisma.$connect().then(() => {
-  prisma.user.findMany().then((res) => {
-    console.log(res);
-  });
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 for(var r in resources) {
   new resources[r](app);
 }
 
+function mongoErrorHandler (err, req, res, next) {
+    res
+      .status(500)
+      .json({ error: err.message });
+}
+
+app.use(mongoErrorHandler);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
