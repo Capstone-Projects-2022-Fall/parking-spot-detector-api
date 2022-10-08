f149de67f046f124bb4dfb969de1675450520726
Yuri Yakymiv <ypyakymiv@gmail.com>
2022-10-08 15:17:00 -0400

    test: added basic test for user-controller

---
+++ test/resources/user.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process');
+const request = require('supertest');
+const { expect } = require('chai');
+
+
+var app;
+
+describe('UserController', function() {
+  // Called once before any of the tests in this block begin.
+  beforeEach(async () => {
+    await exec('../../scripts/drop_database_table.sh');
+    app = require('../../src');
+  });
+
+  it('should send back a valid JSON object', async () =>
+    request(app)
+      .get('/users')
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+  );
+
+});
