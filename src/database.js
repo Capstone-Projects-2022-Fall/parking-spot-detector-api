46897a4aa788daa1a0fc8b1224e0c92b3f970c30
Darlin Kola <dkola@gmail.com>
2022-10-08 00:53:55 -0400

    feature: added install script for dev dependencies

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://parkingspotdetectorapi:password@127.0.0.1:27017/parkingspotdetector"; //process.env.DATABASE_URL;
+const URI = "mongodb://127.0.0.1:27017/parkingspotdetector"; //process.env.DATABASE_URL;
 
 async function connect_to_db() {
   return mongoose.connect(URI);
