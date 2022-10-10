a45f42da5b8a9381b32fe0763063ac3f8fc574b6
Yuri Yakymiv <ypyakymiv@gmail.com>
2022-10-10 16:38:47 -0400

    feature: added frame endpoints

---
+++ src/model/frame/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const FrameSchema = new mongoose.Schema({
+  camera_id: {type: String, required: true, index: true, unique: true},
+}, {strict: true});
+
+const Frame = new mongoose.model('Frame', FrameSchema);
+
+module.exports = Frame;
--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,9 @@
 const User = require('./users');
 const Camera = require('./Camera');
+const Frame = require('./Frame');
 
 module.exports = {
   User,
-  Camera
+  Camera,
+  Frame
 };
