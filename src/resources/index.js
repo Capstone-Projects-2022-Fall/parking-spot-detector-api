7ff544e065ed2cb36f476f91dbbd870f5e8d6bb8
Yuri Yakymiv <ypyakymiv@gmail.com>
2022-10-16 15:36:57 -0400

    feature: added register/login endpoints

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,9 +1,11 @@
 const user = require('./user');
 const camera = require('./camera');
 const frame = require('./frame');
+const auth = require('./auth');
 
 module.exports = {
   user,
   camera,
-  frame
+  frame,
+  auth
 };
