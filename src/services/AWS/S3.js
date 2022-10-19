97186c94df3fbec9dd79836c9f403ce7e786cb11
Yuri Yakymiv <ypyakymiv@gmail.com>
2022-10-19 14:49:38 -0400

    feature: added bindings for S3 integration

---
+++ src/services/AWS/S3.js
@@ -0,0 +1,72 @@
+const AWS = require('./aws');
+
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+const BUCKET_NAME = "aws-codestar-us-east-1-424135468230";
+
+async function get_buckets() {
+  return new Promise((resolve, reject) => {
+    s3.listBuckets(function(err, data) {
+      if (err) {
+        // perhaps throw an error
+        reject(err);
+      } else {
+        resolve(data.Buckets);
+      }
+    });
+  });
+}
+
+
+async function bucket_exists() {
+  return new Promise((resolve, reject) => {
+    s3.headBucket({
+      Bucket: BUCKET_NAME
+    }, function(err, data) {
+      if(err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+async function create_bucket() {
+  return new Promise((resolve, reject) => {
+    s3.createBucket({
+      Bucket: BUCKET_NAME
+    }, function(err, data) {
+      if(err) {
+        console.log(err);
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+async function upload_frame(camera_id, frame_id, file) {
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+      Body: file,
+      Bucket: BUCKET_NAME,
+      Key: `frames/${camera_id}/${frame_id}`
+    }, function(err, data) {
+      if(err) {
+        console.log(err);
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+module.exports = {
+  get_buckets,
+  bucket_exists,
+  create_bucket,
+  upload_frame
+};
